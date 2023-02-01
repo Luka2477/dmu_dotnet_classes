3d23d740caf122e7382b6e8ce7164e606c8db9ad
luka2477 <luke.da.duke@hotmail.com>
2023-02-01 09:34:12 +0100

    given

---
+++ day02_constructs/given/TryParse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace given
+{
+    internal class TryParse
+    {
+        static void Main(string[] args)
+        {
+            String tekstAtParse = "3x2";
+            bool virkedeDet = int.TryParse(tekstAtParse, out int result);
+            Console.WriteLine("Virkede det? " + virkedeDet);
+            if (virkedeDet)
+            {
+                Console.WriteLine("Resultat: " + result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
