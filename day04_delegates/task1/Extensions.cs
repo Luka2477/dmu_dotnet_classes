305420c335bdb2ce93ecafce18f6427ce28cf929
luka2477 <luke.da.duke@hotmail.com>
2023-02-07 18:46:46 +0100

    init day04, task 1

---
+++ day04_delegates/task1/Extensions.cs
@@ -0,0 +1,11 @@
+﻿namespace task1
+{
+	public static class Extensions
+	{
+		public static bool Long(this string str, int n)
+		{
+			return str.Length > n;
+		}
+	}
+}
+
---
+++ day04_delegates/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace task1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string longString = "I am a long string";
+        Console.WriteLine(longString.Long(10));
+
+        string shortString = "Oops";
+        Console.WriteLine(shortString.Long(6));
+    }
+}
+
