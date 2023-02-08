e22a82c44b1e11a269ac2db651bd695456fcb781
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-08 10:02:18 +0100

    task 3.a-b

---
+++ day04_delegates/task3/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace task3
+{
+	public static class Extensions
+	{
+		public static int Factorial(this int n)
+		{
+			return Factorial(n - 1, n);
+		}
+
+		public static int Factorial(int n, int sum)
+		{
+			if (n <= 0) return sum;
+
+			return Factorial(n - 1, sum * n);
+		}
+	}
+}
+
---
+++ day04_delegates/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace task3;
+class Program
+{
+	static void Main(string[] args)
+	{
+		//Console.WriteLine(Factorial(5));
+		Console.WriteLine(5.Factorial());
+	}
+
+	//public static int Factorial(int n)
+	//{
+	//	return Factorial(n - 1, n);
+	//}
+
+	//public static int Factorial(int n, int sum)
+	//{
+	//	if (n <= 0) return sum;
+
+	//	return Factorial(n - 1, sum * n);
+	//}
+}
+
