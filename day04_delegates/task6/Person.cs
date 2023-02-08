477124391b96aea2287c8496f916c85c21f94365
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-08 17:14:15 +0100

    task 6

---
+++ day04_delegates/task6/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace task6
+{
+	public class Person
+	{
+		public int Age { get; set; }
+		public double Weight { get; set; }
+		public string Name { get; set; }
+
+		public Person(int age, double weight, string name)
+		{
+			Age = age;
+			Weight = weight;
+			Name = name ?? throw new ArgumentNullException(nameof(name));
+		}
+
+		public override string ToString()
+		{
+			return $"I am {Name}, I weigh {Weight}kg and am {Age} years old...";
+		}
+	}
+
+	public class ByAgeSorting : IComparer<Person>
+	{
+		public int Compare(Person? x, Person? y)
+		{
+			return x.Age.CompareTo(y.Age);
+		}
+	}
+
+	public class ByWeightSorting : IComparer<Person>
+	{
+		public int Compare(Person? x, Person? y)
+		{
+			return x.Weight.CompareTo(y.Weight);
+		}
+	}
+
+	public class ByNameSorting : IComparer<Person>
+	{
+		public int Compare(Person? x, Person? y)
+		{
+			return x.Name.CompareTo(y.Name);
+		}
+	}
+}
+
---
+++ day04_delegates/task6/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace task6;
+class Program
+{
+	static void Main(string[] args)
+	{
+		List<Person> people = new()
+		{
+			new Person(22, 80, "Lukas"),
+			new Person(60, 105, "Per"),
+			new Person(26, 91, "Lasse")
+		};
+
+		//people.Sort(new ByAgeSorting());
+		//PrintList<Person>(people);
+
+		//people.Sort(new ByWeightSorting());
+		//PrintList<Person>(people);
+
+		//people.Sort(new ByNameSorting());
+		//PrintList<Person>(people);
+
+		people.Sort((p1, p2) => p1.Age.CompareTo(p2.Age));
+		PrintList<Person>(people);
+
+		people.Sort((p1, p2) => p1.Weight.CompareTo(p2.Weight));
+		PrintList<Person>(people);
+
+		people.Sort((p1, p2) => p1.Name.CompareTo(p2.Name));
+		PrintList<Person>(people);
+	}
+
+	static void PrintList<T>(List<T> list)
+	{
+		foreach (T element in list)
+		{
+			Console.WriteLine(element);
+		}
+		Console.WriteLine();
+	}
+}
+
