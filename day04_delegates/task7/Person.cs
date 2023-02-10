890b7c0320bc6dbea2a8bc0a053890c904e9718c
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-10 16:59:30 +0100

    done: task 7

---
+++ day04_delegates/task7/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+	public delegate string FormatPersonName(string firstname, string lastname);
+
+	public class Person
+	{
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+
+		public static List<Person> CreateListOfPeople()
+		{
+			return new List<Person>()
+						{
+								new Person() { FirstName = "Stephen", LastName = "King" },
+								new Person() { FirstName = "George", LastName = "Martin" },
+								new Person() { FirstName = "Ernest", LastName = "Hemingway" },
+								new Person() { FirstName = "William", LastName = "Shakespeare" }
+						};
+		}
+
+		public void PrintPersonName(FormatPersonName formatter)
+		{
+			Console.WriteLine(formatter(FirstName, LastName));
+		}
+	}
+}
---
+++ day04_delegates/task7/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace task7;
+class Program
+{
+	public static void Main(string[] args)
+	{
+		List<Person> people = Person.CreateListOfPeople();
+
+		while (true)
+		{
+			Console.WriteLine("----------------------------------------------------");
+			Console.WriteLine("Hvordan vil du have udskrevet navnene?");
+			Console.WriteLine("1: Efternavn efterfulgt af fornavn");
+			Console.WriteLine("2: Efternavn efterfulgt af fornavn, store bogstaver");
+			Console.WriteLine("3: Efternavn efterfulgt af fornavn, små bogstaver");
+			Console.WriteLine("4: Kun forbogstav af fornavn efterfulgt af efternavn");
+			Console.WriteLine();
+			Console.WriteLine("q: For at afslutte!");
+			Console.Write("> ");
+			string? input = Console.ReadLine();
+			switch (input?.ToUpper())
+			{
+				case "1":
+					PrintPeople(people, (fname, lname) => $"{lname} {fname}");
+					break;
+				case "2":
+					PrintPeople(people, (fname, lname) => $"{lname.ToUpper()} {fname.ToUpper()}");
+					break;
+				case "3":
+					PrintPeople(people, (fname, lname) => $"{lname.ToLower()} {fname.ToLower()}");
+					break;
+				case "4":
+					PrintPeople(people, (fname, lname) => $"{lname.ToUpper()[0]} {fname}");
+					break;
+				case "Q": return;
+				default:
+					Console.WriteLine("Ukendt valg, prøv igen");
+					break;
+			}
+		}
+	}
+
+	public static void PrintPeople(List<Person> people, FormatPersonName formatter)
+	{
+		foreach (Person person in people)
+		{
+			person.PrintPersonName(formatter);
+		}
+	}
+}
+
