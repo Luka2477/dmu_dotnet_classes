f3ba23e4246e774e58aaa5d8ea8c2df3a33f762a
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-15 10:30:41 +0100

    feature: done task 1

--- day05_linq/task1/Program.cs
+++ day05_linq/task1/Program.cs
@@ -1,9 +1,37 @@
 ﻿namespace task1;
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+	static void Main(string[] args)
+	{
+		List<int> ints = new List<int> { 10, 12, 2143, 345, 456, 56, 678, 4, 3 };
+
+		ints.ForEach((val) =>
+		{
+			if (val % 2 == 0)
+			{
+				Console.WriteLine(val);
+			}
+		});
+
+		int temp = 0;
+		ints.ForEach((val) =>
+		{
+			if (val > 15)
+			{
+				temp = val;
+			}
+		});
+		Console.WriteLine(temp);
+
+		int index = 0;
+		ints.ForEach((val) =>
+		{
+			if (val > 15)
+			{
+				index = ints.IndexOf(val);
+			}
+		});
+		Console.WriteLine(index);
+	}
 }
 
