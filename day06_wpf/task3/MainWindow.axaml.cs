f7d731952a8a3a78aa36573a82ffc80313af05f3
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-21 10:04:33 +0100

    feature: done task 3

--- day06_wpf/task3/MainWindow.axaml.cs
+++ day06_wpf/task3/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace task3;
 
@@ -7,5 +8,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void ButtonHello_OnClick(object? sender, RoutedEventArgs e)
+    {
+        ButtonHello.Content = "I have been clicked!";
     }
 }
