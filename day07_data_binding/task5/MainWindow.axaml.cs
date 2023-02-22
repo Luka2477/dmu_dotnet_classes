1afa5e12ed8b1c8a572cb6e14af06a633e9a1e1b
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-22 11:14:51 +0100

    feature: init task 5

---
+++ day07_data_binding/task5/MainWindow.axaml.cs
@@ -0,0 +1,28 @@
+using System.Collections.ObjectModel;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace task5;
+
+public partial class MainWindow : Window
+{
+    private readonly ObservableCollection<PersonDataContext> _persons;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        _persons = new ObservableCollection<PersonDataContext>
+        {
+            new("Lukas", 80.5, 23, 99.9, true),
+            new("Mads", 75.0, 26, 75.0, true),
+            new("Mike", 90.0, 28, 50.0, false)
+        };
+        LbPersons.DataContext = _persons;
+    }
+
+    private void BtnAddPerson_OnClick(object? sender, RoutedEventArgs e)
+    {
+        _persons.Add(new PersonDataContext("New Person", 10.0, 10, 10.0, true));
+    }
+}
