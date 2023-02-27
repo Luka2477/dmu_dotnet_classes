a7e6dfc463869e386406b42c03bd7584849ef839
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-27 22:25:53 +0100

    feature: done task 4

--- day08_entity_framework/task4/MainWindow.axaml.cs
+++ day08_entity_framework/task4/MainWindow.axaml.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Linq;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
 using Microsoft.EntityFrameworkCore;
 using task4.DAL;
 using task4.Models;
@@ -18,6 +18,23 @@ public partial class MainWindow : Window
         Console.WriteLine($"Database created: {_context.Database.EnsureCreated()}");
         Console.WriteLine($"Database connected: {_context.Database.CanConnect()}");
         
-        
+        _context.Cars.Load();
+        ComboBoxCars.Items = _context.Cars.Local.ToObservableCollection();
+        ComboBoxCars.SelectedIndex = 0;
+
+        _context.Owners.Load();
+        ListBoxOwners.Items = _context.Cars.ToList()[0].Owners;
+    }
+
+    private void ComboBoxCars_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        Car selected = (Car)ComboBoxCars.SelectedItem;
+        if (selected == null)
+        {
+            ListBoxOwners.Items = null;
+            return;
+        }
+
+        ListBoxOwners.Items = selected.Owners;
     }
 }
