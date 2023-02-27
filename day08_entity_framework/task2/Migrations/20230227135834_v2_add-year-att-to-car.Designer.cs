5f122899d326e3e23611c11d84ee5f4d266a31e9
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-27 14:59:36 +0100

    feature: second mig

--- day08_entity_framework/task2/Migrations/20230227135834_v2_add-year-att-to-car.Designer.cs
+++ day08_entity_framework/task2/Migrations/20230227135834_v2_add-year-att-to-car.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using task2.DAL;
 
@@ -10,9 +11,11 @@ using task2.DAL;
 namespace task2.Migrations
 {
     [DbContext(typeof(CarContext))]
-    partial class CarContextModelSnapshot : ModelSnapshot
+    [Migration("20230227135834_v2_add-year-att-to-car")]
+    partial class v2_addyearatttocar
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
