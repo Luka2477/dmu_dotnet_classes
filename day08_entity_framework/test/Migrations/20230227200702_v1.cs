47612987169e864edd85137349c1f2b8dc9e641e
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-27 21:08:05 +0100

    feature: mig v1

---
+++ day08_entity_framework/test/Migrations/20230227200702_v1.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace test.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Houses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Houses", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Houses_Persons_OwnerID",
+                        column: x => x.OwnerID,
+                        principalTable: "Persons",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Pets_Persons_OwnerID",
+                        column: x => x.OwnerID,
+                        principalTable: "Persons",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Persons",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Lukas Knudsen" },
+                    { 2, "Mads Bjerg" },
+                    { 3, "Berta Vazquez" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Houses",
+                columns: new[] { "ID", "Address", "OwnerID" },
+                values: new object[,]
+                {
+                    { 1, "Aarhus", 1 },
+                    { 2, "København", 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pets",
+                columns: new[] { "ID", "Name", "OwnerID" },
+                values: new object[,]
+                {
+                    { 1, "Bailey", 1 },
+                    { 2, "Cody", 3 },
+                    { 3, "Maggie", 3 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Houses_OwnerID",
+                table: "Houses",
+                column: "OwnerID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_OwnerID",
+                table: "Pets",
+                column: "OwnerID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Houses");
+
+            migrationBuilder.DropTable(
+                name: "Pets");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
