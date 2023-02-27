1064bccd63b4ac0720c24568da130cc59c2b0e38
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-28 00:27:09 +0100

    feature: almost done...

--- day08_entity_framework/task6/MainWindow.axaml.cs
+++ day08_entity_framework/task6/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 using System;
 using Avalonia.Controls;
+using Microsoft.EntityFrameworkCore;
 using task6.DAL;
+using task6.Models;
 
 namespace task6;
 
@@ -14,5 +16,35 @@ public partial class MainWindow : Window
 
         Console.WriteLine($"Database created: {_context.Database.EnsureCreated()}");
         Console.WriteLine($"Database connected: {_context.Database.CanConnect()}");
+        
+        _context.Students.Load();
+        ListBoxStudents.Items = _context.Students.Local.ToObservableCollection();
+        
+        _context.Classes.Load();
+    }
+
+    private void ListBoxStudents_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        Student selected = (Student)ListBoxStudents.SelectedItem;
+        if (selected == null)
+        {
+            ListBoxClasses.Items = null;
+            ListBoxExtra.Items = null;
+            return;
+        }
+
+        ListBoxClasses.Items = selected.Classes;
+    }
+
+    private void ListBoxClasses_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        Class selected = (Class)ListBoxClasses.SelectedItem;
+        if (selected == null)
+        {
+            ListBoxExtra.Items = null;
+            return;
+        }
+
+        ListBoxExtra.Items = selected.Students;
     }
 }
