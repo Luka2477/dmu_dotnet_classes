ab3faa7ac6a351561486787274185216e65b837b
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-27 12:22:00 +0100

    feature: done task 1

---
+++ day08_entity_framework/task1/MainWindow.axaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using task1.DAL;
+using task1.Models;
+
+namespace task1;
+
+public partial class MainWindow : Window
+{
+    private readonly CarContext _context = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Console.WriteLine($"Database created: {_context.Database.EnsureCreated()}");
+        Console.WriteLine($"Database connected: {_context.Database.CanConnect()}");
+
+        LbCars.Items = _context.Cars;
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        _context.Cars.Add(new Car("Skoda", "Octavia", 4));
+        _context.SaveChanges();
+
+        LbCars.Items = null;
+        LbCars.Items = _context.Cars;
+    }
+}
---
+++ day08_entity_framework/task1/Models/Car.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace task1.Models;
+
+public class Car
+{
+    public int ID { get; set; }
+    public string Make { get; set; } = "";
+    public string Model { get; set; } = "";
+    public int Wheels { get; set; }
+
+    public Car()
+    {
+    }
+    
+    public Car(string make, string model, int wheels)
+    {
+        Make = make ?? throw new ArgumentNullException(nameof(make));
+        Model = model ?? throw new ArgumentNullException(nameof(model));
+        Wheels = wheels;
+    }
+
+    public override string ToString()
+    {
+        return $"Car {{ID={ID}, Make='{Make}', Model='{Model}', Wheels={Wheels}}}";
+    }
+}
