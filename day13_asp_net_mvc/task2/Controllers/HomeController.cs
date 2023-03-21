497ab59bb083f807587de75c7ddc692ea82ff8d5
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-03-21 09:46:54 +0100

    feature: done task 2

--- day13_asp_net_mvc/task2/Controllers/HomeController.cs
+++ day13_asp_net_mvc/task2/Controllers/HomeController.cs
@@ -10,8 +10,22 @@ public class HomeController : Controller
     {
         ViewBag.Persons = new List<Person>
         {
-            new("Lukas", "Knudsen", "Something 1", "8000", "Aarhus"),
+            new("Lukas", "Knudsen", "Something 1", "8000", "Aarhus")
+            {
+                PhoneNumbers =
+                {
+                    "12345678",
+                    "11223344"
+                },
+                Birthday = new DateTime(2000, 02, 19)
+            },
             new("Berta", "Vazquez", "Something 2", "8000", "Aarhus")
+            {
+                PhoneNumbers =
+                {
+                    "98765432"
+                }
+            }
         };
         return View();
     }
--- day13_asp_net_mvc/task2/Models/Person.cs
+++ day13_asp_net_mvc/task2/Models/Person.cs
@@ -2,12 +2,31 @@ namespace task2.Models;
 
 public class Person
 {
+    private DateTime _birthday;
+
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Address { get; set; }
     public string Zip { get; set; }
     public string City { get; set; }
-    public string PhoneNumber { get; set; } = "NULL";
+
+    public DateTime Birthday
+    {
+        get => _birthday;
+        init
+        {
+            DateTime temp = _birthday;
+            _birthday = value;
+
+            if (Age is > 0 and < 120) return;
+
+            _birthday = temp;
+            throw new ArgumentException("Age must be between 0 and 120 years");
+        }
+    }
+
+    public int Age => (int)((DateTime.Now - Birthday).TotalDays / 365);
+    public List<string> PhoneNumbers { get; set; } = new();
 
     public Person(string firstName, string lastName, string address, string zip, string city)
     {
@@ -16,5 +35,10 @@ public class Person
         Address = address ?? throw new ArgumentNullException(nameof(address));
         Zip = zip ?? throw new ArgumentNullException(nameof(zip));
         City = city ?? throw new ArgumentNullException(nameof(city));
+    }
+
+    public void AddPhoneNumber(string pn)
+    {
+        PhoneNumbers.Add(pn);
     }
 }
